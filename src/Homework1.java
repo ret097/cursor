9ff7980a8f61d642b409bc465a821e4a14ae3fb9
ret097 <shklyodik@gmail.com>
2019-11-30 20:38:52 +0200

    hw1

---
+++ src/Homework1.java
@@ -0,0 +1,13 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        byte a = 8;
+        short b = 16;
+        long c = 64;
+        double d;
+        float e;
+        d = (c + b) - a;
+        e = (c * a) / b;
+        System.out.println("double = " + d);
+        System.out.println("float = " + e);
+    }
+}
